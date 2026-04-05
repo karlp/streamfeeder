31f10fd2a8c13bedfc0de1f171311e3768d0277b
Karl Palsson <karlp@tweak.au>
2026-04-05 00:01:46 +0000

    we have webdav lists!

--- streamfeeder/src/views/ListDav1.vue
+++ streamfeeder/src/views/ListDav1.vue
@@ -1,3 +1,28 @@
 <template>
     <h3>This is my dav list page</h3>
+
+    <li>
+        
+    </li>
 </template>
+
+<script lang="ts">
+
+import { createClient } from 'webdav';
+
+const davEndpoint = import.meta.env.VITE_SF_DAV_ENDPOINT;
+const davUsername = import.meta.env.VITE_SF_DAV_USERNAME;
+const davPassword = import.meta.env.VITE_SF_DAV_PASSWORD;
+
+const client = createClient(
+    davEndpoint,
+    {
+        username: davUsername,
+        password: davPassword
+    }
+);
+
+const directoryItems = await client.getDirectoryContents("/");
+console.log(directoryItems);
+
+</script>
